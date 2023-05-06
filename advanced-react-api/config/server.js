c6f120e763f298c02bd2193cf0bdb6f28eb8ecb6
Author Name <juliograffin@gmail.com>
2023-05-05 21:37:17 -0300

    Initial commit from Create Next App

---
+++ advanced-react-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4362dccac2ff9c23e973881641f6904a'),
+    },
+  },
+});
---
+++ advanced-react-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '7bdd6814-7094-42c0-9552-20900bd8f80a'
+};
